a1ae290ff18641d14df16166865ff5e05be6a76d
GONZALO DEVILLE <gonza181092@gmail.com>
2020-11-09 00:37:00 -0300

    Most proudly of this implementation.

--- src/main/java/es/exercise/Solution.java
+++ src/main/java/es/exercise/Solution.java
@@ -1,11 +1,11 @@
 package es.exercise;
 
+import es.exercise.pojo.Product;
 import es.exercise.pojo.ProductSales;
 import es.exercise.pojo.ProductStock;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Solution {
     
@@ -16,27 +16,52 @@ public class Solution {
         if(productsStockInformation.isEmpty()) throw new IllegalArgumentException("List of Product stock is empty");
         if(productsSalesInformation.isEmpty()) throw new IllegalArgumentException("List of Product sales is empty");
         
+        /*flat stock list on product list*/
+        List<Product> products = productsStockInformation.stream()
+                .map(p -> new Product(p.getProductId(),0d,p.getAvailableStock()))
+                .collect(Collectors.toList());
+        
+        /*update flatten list with sales amount, and get both list flattened*/
+        products.forEach(product ->
+                product.setSalesAmount(
+                        productsSalesInformation.stream()
+                        .filter(sales -> sales.getProductId().equals(product.getProductId()))
+                        .map(ProductSales::getSalesAmount)
+                        .findAny().orElse(0D)
+                )
+        );
+        
+        Comparator<Product> compareBySales = Comparator.comparing(Product::getSalesAmount).reversed();
+        Comparator<Product> compareByStock = Comparator.comparing(Product::getAvailableStock).reversed();
+        
         List<Long> result;
         if(stockWeight > salesWeight){
-            result = doOrderByStock(productsStockInformation);
+            result = doOrderByStock(products,compareBySales,compareByStock);
         }else if (stockWeight < salesWeight){
-            result = doOrderBySales(productsSalesInformation);
+            result = doOrderBySales(products,compareBySales,compareByStock);
         }else{
-            result = doOrderByStock(productsStockInformation);//default case
+            result = doOrderByStock(products,compareBySales,compareByStock);//default case
         }
+//        System.out.println(result);
         return result;
     }
     
-    private static List<Long> doOrderBySales(List<ProductSales> productsSalesInformation) {
-        Comparator<ProductSales> compareBySales = Comparator.comparing(ProductSales::getSalesAmount);
-        List<ProductSales> sorted = productsSalesInformation.stream().sorted(compareBySales.reversed()).collect(Collectors.toList());
-        return sorted.stream().map(ProductSales::getProductId).collect(Collectors.toList());
+    private static List<Long> doOrderBySales(List<Product> products, Comparator<Product> compareBySales, Comparator<Product> compareByStock) {
+        List<Product> sorted = products.stream()
+                .sorted(compareBySales.thenComparing(compareByStock))
+                .collect(Collectors.toList());
+        return getSortedIds(sorted);
     }
     
-    private static List<Long> doOrderByStock(List<ProductStock> productsStockInformation) {
-        Comparator<ProductStock> compareByStock = Comparator.comparing(ProductStock::getAvailableStock);
-        List<ProductStock> sorted = productsStockInformation.stream().sorted(compareByStock.reversed()).collect(Collectors.toList());
-        return sorted.stream().map(ProductStock::getProductId).collect(Collectors.toList());
+    private static List<Long> doOrderByStock(List<Product> products, Comparator<Product> compareBySales, Comparator<Product> compareByStock) {
+        List<Product> sorted = products.stream()
+                .sorted(compareByStock.thenComparing(compareBySales))
+                .collect(Collectors.toList());
+        return getSortedIds(sorted);
+    }
+    
+    private static List<Long> getSortedIds(List<Product> sorted) {
+        return sorted.stream().map(Product::getProductId).collect(Collectors.toList());
     }
     
 }
